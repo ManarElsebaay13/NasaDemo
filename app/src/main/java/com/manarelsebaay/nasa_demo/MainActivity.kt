e5da6995792be2ceb93d9ef377bd4505fc147731
manarelsebaay <manarelsebaay13@gmail.com>
2022-10-25 17:32:04 +0200

    init

---
+++ app/src/main/java/com/manarelsebaay/nasa_demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.manarelsebaay.nasa_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //init
+        setContentView(R.layout.activity_main)
+    }
+}
